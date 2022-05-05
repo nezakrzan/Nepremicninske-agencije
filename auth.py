c6f478216b52b502972a1ba4ed11bcb05684e955
Ana Marija Okorn <ana-marija.okorn@student.fmf.uni-lj.si>
2022-05-05 12:44:38 +0200

    dodan auth.py

---
+++ auth.py
@@ -0,0 +1,14 @@
+db = 'sem2022_anamarijao'
+host = 'baza.fmf.uni-lj.si'
+user = 'ana'
+password = 'ana123'
+
+db = 'sem2022_anamarijao'
+host = 'baza.fmf.uni-lj.si'
+user = 'nina'
+password = 'nina123'
+
+db = 'sem2022_anamarijao'
+host = 'baza.fmf.uni-lj.si'
+user = 'neza'
+password = 'neza123'
