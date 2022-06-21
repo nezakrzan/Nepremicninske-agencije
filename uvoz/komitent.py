c8afc313a44126466a753c4a3ee5c5c01826204f
Nina2809 <49915013+Nina2809@users.noreply.github.com>
2022-06-21 12:20:24 +0200

    sprememba tabel

--- uvoz/komitent.py
+++ uvoz/komitent.py
@@ -39,7 +39,7 @@ def uvozi_podatke():
 conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 
-pobrisi_tabelo()
-ustvari_tabelo()
+#pobrisi_tabelo()
+#ustvari_tabelo()
 uvozi_podatke()
 
